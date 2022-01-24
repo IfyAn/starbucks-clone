59c232ec402879930afa0ade588039db3e36aafc
Anyam Ifeanyi <chukwuanyan@gmail.com>
2022-01-24 18:48:07 +0100

    first commit

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,15 @@
 import firebase from 'firebase'
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDFPOEapzE2otnYAjcgGRf-gc7tbK0MX00',
-  authDomain: 'starbucks-clone.firebaseapp.com',
-  projectId: 'starbucks-clone',
-  storageBucket: 'starbucks-clone.appspot.com',
-  messagingSenderId: '852007719923',
-  appId: '1:852007719923:web:4c57f4ef9782f81026f886',
-}
+  apiKey: "AIzaSyBrDi4AZXojhjtg5WEYehGDTV5sLK4LiV4",
+  authDomain: "people-drink-4a53c.firebaseapp.com",
+  projectId: "people-drink-4a53c",
+  storageBucket: "people-drink-4a53c.appspot.com",
+  messagingSenderId: "302997537820",
+  appId: "1:302997537820:web:ef1e292a840c3bad848bd9",
+  measurementId: "G-Q4GTXYR0BT"
+};
+
 
 const firebaseApp = firebase.initializeApp(firebaseConfig)
 
